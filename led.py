014a963a8d7c19863ef5f90b241b73b693660ffb
Nordlxnder <lnxawdr@web.de>
2017-08-29 17:43:24 +0200

    Testskript led.py eingefügt

--- led.py
+++ led.py
@@ -7,7 +7,6 @@
 	Die LEDs leuten für 2 s Grün-Gelb und gehen dann aus  
 '''
 
-
 import spidev
 import time
  
@@ -15,12 +14,12 @@ DELAY = 0.04
 LED_COUNT = 30
 spi = spidev.SpiDev()
 
-# Starteinstellungen für die Schnittstelle
+# Starteinstellungen für die Schnittstelle  CS0
 spi.open(0, 0)  
 spi.mode = 0b00
 spi.max_speed_hz= 500000
 
-# Status  Einstellungen der Parameter für die Schnittstelle
+# Statusanzeige der  Einstellungen der Parameter für die Schnittstelle
 bpw=spi.bits_per_word
 cshigh= spi.cshigh
 lsbfirst = spi.lsbfirst
@@ -34,10 +33,12 @@ print("Speed:\t\t", speed,"\nMode:\t\t", mode ,"\nThreewire:\t",tw)
 spi.writebytes([0xB1, LED_COUNT, 24])
 time.sleep(DELAY)
 
-# Einschalten aller LEDs
+# Einschalten aller LEDs und Festlegung der Farben 
 spi.writebytes([0xA1, 255, 255, 0, 0xA5, 0xB2])
 time.sleep(2)
 
 # Ausschalten aller LEDs
 spi.writebytes([0xA1, 0,0, 0, 0xA5, 0xB2])
 time.sleep(DELAY)
+
+print("Ich habe fertig!")
